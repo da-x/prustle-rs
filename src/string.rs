d2e98051c61de651ed77812b5eef1e4c19a41444
Dan Aloni <alonid@gmail.com>
2017-03-18 17:55:35 +0200

    Add some builtins in prustle::string

--- src/string.rs
+++ src/string.rs
@@ -11,3 +11,23 @@ pub fn upper(s : &EZString) -> EZString {
 pub fn lower(s : &EZString) -> EZString {
     s.to_lowercase()
 }
+
+// Some global builtins that are not locale-dependent:
+
+#[allow(non_upper_case_globals)]
+pub static ascii_letter: &'static str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+#[allow(non_upper_case_globals)]
+pub static ascii_lowercase: &'static str = "abcdefghijklmnopqrstuvwxyz";
+
+#[allow(non_upper_case_globals)]
+pub static ascii_upppecase: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+#[allow(non_upper_case_globals)]
+pub static digits: &'static str = "0123456789";
+
+#[allow(non_upper_case_globals)]
+pub static hexdigits: &'static str = "0123456789abcdefABCDEF";
+
+#[allow(non_upper_case_globals)]
+pub static octdigits: &'static str = "01234567";
